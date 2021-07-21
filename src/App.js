d9b1869d13d9b5878c22cab9863ce2f31bd4e4de
David <d352wang@uwaterloo.ca>
2021-07-20 23:04:55 -0400

    added arrow keys and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import publish from "./components/publish.js";
+import { AiFillCaretDown, AiFillCaretLeft, AiFillCaretRight, AiFillCaretUp } from "react-icons/ai";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <button className="left" onClick={publish}>
+          <AiFillCaretLeft className="arrow-icon" size={40}/>
+        </button>
+        <button className="right" onClick={publish}>
+          <AiFillCaretRight className="arrow-icon" size={40}/>
+        </button>
+      </div>);
+  }
+
 }
 
 export default App;
